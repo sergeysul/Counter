7e1eae1a9d3718c843cf12e79020fe9e2e60e4df
elc <hm8huh@mail.ru>
2024-05-01 22:48:53 +0300

    Изменен вывод истории

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -2,18 +2,56 @@
 //  ViewController.swift
 //  Counter
 //
-//  Created by Сергей Сулятецкий on 27.04.2024.
-//
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var plusButton: UIButton!
+    @IBOutlet weak var minusButton: UIButton!
+    @IBOutlet weak var resetButton: UIButton!
+    @IBOutlet weak var placeTextView: UITextView!
+    @IBOutlet weak var counterLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        placeTextView.text = "История изменений:"
+        placeTextView.isEditable = false
+        updateCounter()
     }
-
-
+    var counter = 0
+    
+    @IBAction func plus(_ sender: Any) {
+        counter += 1
+        updateCounter()
+        placeTextView.text += "\n[\(date())]: Значение изменено на +1"
+        
+    }
+    @IBAction func minus(_ sender: Any) {
+        if counter > 0{
+            counter -= 1
+            placeTextView.text += "\n[\(date())]: Значение изменено на -1"
+        }else{
+            placeTextView.text += "\n[\(date())]: Попытка уменьшить значение счетчика ниже 0"
+        }
+        updateCounter()
+    }
+    
+    @IBAction func reset(_ sender: Any) {
+        counter = 0
+        placeTextView.text += "\n[\(date())]: Значение сброшено"
+        updateCounter()
+    }
+    
+    func updateCounter(){
+        counterLabel.text = "Значение счетчика: \(counter)"
+    }
+    
+    func date() -> String{
+        let formatter = DateFormatter()
+        formatter.dateFormat = "dd.MM.yyyy HH:mm:ss"
+        return formatter.string(from: Date())
+    }
 }
 
